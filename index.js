f2ce0977bfadf7dfca89af64522120781994bfa0
Markus Tacker <m@coderbyheart.com>
2017-07-11 21:36:31 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var xkcdPassword = require('xkcd-password')
+var pw = new xkcdPassword()
+
+var options = {
+    numWords: 4,
+    minLength: 5,
+    maxLength: 8
+}
+
+pw.generate(options).then(function (result) {
+    console.log(result.join(' '))
+})
